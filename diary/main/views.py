3dc5988f02ade070f126923c3c736fe4617e46b1
Elliagh <ivilglbo@gmail.com>
2023-02-23 14:40:27 +0300

    making request handlers

--- diary/main/views.py
+++ diary/main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-#def say_hello(request):
-#    return render(request, 'main/say_hello.html', {})
+def index(request):
+    return HttpResponse('<h2>hello world</h2>')
+
+def add_task(request):
+    return HttpResponse('<h3>Добавить задачу</h2>')
+
+def get_result(request):
+    return HttpResponse('<h2>Получить сводку</h2>')
+
+def clear_tasks(request):
+    return HttpResponse('<h2>Очистить список</h2>')
+
+def delete_task(request):
+    return HttpResponse('<h2>Удалить задачу</h2>')
